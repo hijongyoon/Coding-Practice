bb8aa9bfd3e39202b545f7a72da4860ebf8081ea
hijongyoon <darkknight35@nate.com>
2020-05-28 18:12:46 +0900

    find the best answer

--- delivery of sugar.py
+++ delivery of sugar.py
@@ -4,22 +4,16 @@ import sys
 
 def main():
     num = int(sys.stdin.readline())
-    if num % 5 == 0:
-        print(num // 5)
-    elif (num % 5) == 3:
-        print((num // 5) + 1)
-    elif (num % 5) != 3 and (num // 5) != 0:
-        x = (num // 5) - 1
-        y = (num % 5) + 5
-        if y % 3 == 0:
-            print(x + (y // 3))
-        else:
-            if num % 3 == 0:
-                print(num // 3)
-            else:
-                print("-1")
-    elif (num % 5) != 3 and (num // 5) == 0:
-        print("-1")
+    if num%5==0: print(num//5) # 5로 완전히 나눠지는 것이 가장 좋으므로 바로 출력.
+    elif (num%5)==3: print((num//5)+1) # 5로 나누고 나머지가 3이라면 몫에서 1을 더함.
+    else:
+        num5=num//5
+        rest=num%5
+        while rest%3!=0 and num5>=0: # 나머지에 5를 더하면서 몫은 1씩 뺌. 동시에 나머지가 3에 나눠지거나 몫이 0보다 작아지면 탈출.
+            rest+=5
+            num5-=1
+        if rest%3!=0 or rest>num: print("-1") # 3과 5로 절대 표현할수 없는 수는 위의 반복문을 거치면 나머지가 원래 수 보다 커짐.
+        else: print(num5+rest//3)
 
 
 main()
