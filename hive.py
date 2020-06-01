a242834fafa750e4bf13150832999566349eadfa
hijongyoon <darkknight35@nate.com>
2020-06-01 17:19:48 +0900

    find the shortest way for the answer

--- hive.py
+++ hive.py
@@ -2,11 +2,9 @@ import sys
 
 
 def check(num1, num2, num3):
-    li = list(range(num2, num3 + 1))
-    if li.count(num1) == 0:
-        return False
-    else:
+    if num2<=num1<=num3:
         return True
+    else: return False
 
 
 def main():
@@ -14,16 +12,19 @@ def main():
     i = 2
     num1 = 0
     num2 = 1
+    if num==1:
+        print(num)
+        return
     while True:
         if check(num, num1, num2):
             print(i)
             break
-        elif num1 == 0:
+        elif num1 == 0:#처음의 경우 1개에서 5개만 증가하므로 다른 case 로 분류.
             num1 = num2
-            num2 += 6
+            num2 += 6#최대의 범위.
         else:
-            num1 = num2
-            num2 += (6 * i)
+            num1 = num2#num1은 최소의 범위를 의미.
+            num2 += (6 * i)#갯수가 6개,12개,18개 즉 6의 배수로 늘어남.
             i += 1
 
 
