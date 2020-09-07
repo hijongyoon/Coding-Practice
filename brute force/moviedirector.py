d454bec6231d339a6a03f8a23df734b59925d8f7
hijongyoon <darkknight35@nate.com>
2020-09-07 13:03:20 +0900

    find n'th movie title

--- brute force/moviedirector.py
+++ brute force/moviedirector.py
@@ -4,17 +4,23 @@ import sys
 def main():
     pivot = 666
     i = 0
-    n = int(sys.stdin.readline())-1
+    n = int(sys.stdin.readline()) - 1
+    flag = True
     while i < n:
         pivot += 1000
-        if str(pivot).count('6') > 3 and str(pivot).count('666') >= 1:
+        if str(pivot).count('6') > 3 and str(pivot).count('6666') >= 1:
+            pre_pivot = pivot
             i -= 1
             pivot -= 7
-            for j in range(9):
+            for j in range(11):
                 i += 1
-                if i >= n: break
+                if i >= n:
+                    flag = False
+                    break
                 pivot += 1
+            if flag: pivot = pre_pivot+1000
         i += 1
     print(pivot)
+
 
 main()
