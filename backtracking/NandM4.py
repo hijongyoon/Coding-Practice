eb6e6d16d0be0754ae4f4c87564b6697b343598f
hijongyoon <darkknight35@nate.com>
2020-10-06 13:39:00 +0900

    product(Baekjoon-15652)

--- backtracking/NandM4.py
+++ backtracking/NandM4.py
@@ -1,23 +1,20 @@
 import sys
-import itertools
 
 
-def to_generate(per):
-    for i in per:
-        yield i
+def per(start, m, s, li):
+    if start >= m:
+        print(" ".join(list(s)))
+        return
+    for i in range(len(li)):
+        if int(s[start-1]) <= li[i]:
+            per(start + 1, m, s + str(li[i]), li)
 
 
 def main():
     n, m = map(int, sys.stdin.readline().split())
-    per = list(itertools.product([x + 1 for x in range(n)], repeat=m))
-    per = list(set([tuple(sorted(x)) for x in per]))
-    per.sort()
-    per = to_generate(per)
-    for i in per:
-        for j in range(m):
-            print(i[j], end=' ')
-        print()
+    li = list(range(1, n + 1))
+    for i in range(len(li)):
+        per(1, m, str(li[i]), li)
 
 
 main()
-
