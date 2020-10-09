76f383aca932f6b09ab96507eb0f57c898857f1e
hijongyoon <darkknight35@nate.com>
2020-10-09 21:45:54 +0900

    N-queen(Baekjoon-9663)

--- backtracking/N-queen.py
+++ backtracking/N-queen.py
@@ -1,27 +1,36 @@
 import sys
 
 count = 0
+low_li = []
 
 
-def promising(level, cols): # 같은 열이거나 같은 대각선인지 검사하는 것.
+def promising(level, cols):  # 같은 열이거나 같은 대각선인지 검사하는 것.
     for i in range(level):
-        if cols[i] == cols[level]: # 같은 열인가?
+        if cols[i] == cols[level]:  # 같은 열인가?
             return False
-        elif level - i == abs(cols[level] - cols[i]): # 같은 대각선 인가?
+        if level - i == abs(cols[level] - cols[i]):  # 같은 대각선 인가?
             return False
     return True
 
 
-def queen(level, n, cols):
+def queen(level, n, cols, num):
     global count
+    global low_li
     if not promising(level, cols):
         return False
     elif level == n - 1:
+        print(cols)
         count += 1
         return True
     for i in range(n):
+        if level >= 0 and (cols[level] == i or i + 1 == cols[level] or i - 1 == cols[level] or i in low_li):
+            continue
+        if num != -1:
+            low_li.append(num)
         cols[level + 1] = i
-        queen(level + 1, n, cols)
+        queen(level + 1, n, cols, i)
+        if num != -1:
+            low_li.remove(num)
     return False
 
 
@@ -29,7 +38,7 @@ def main():
     global count
     n = int(sys.stdin.readline())
     cols = [-1] * n
-    queen(-1,n,cols)
+    queen(-1, n, cols,-1)
     print(count)
 
 
